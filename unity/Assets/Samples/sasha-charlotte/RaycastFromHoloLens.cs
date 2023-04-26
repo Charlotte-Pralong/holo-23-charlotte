7a26214567353b84219e36be5de7ba753c44e060
Charlotte-Pralong <chacha.pralong@gmail.com>
2023-04-26 11:25:17 +0200

    Create doors on walls

--- unity/Assets/Samples/sasha-charlotte/RaycastFromHoloLens.cs
+++ unity/Assets/Samples/sasha-charlotte/RaycastFromHoloLens.cs
@@ -4,6 +4,8 @@ public class RaycastFromHoloLens : MonoBehaviour
 {
     private float maxDistance = 20.0f;
 
+    public GameObject prefab;
+
     void Update()
     {
         PerformRaycast();
@@ -11,16 +13,28 @@ public class RaycastFromHoloLens : MonoBehaviour
 
     private void PerformRaycast()
     {
-        Vector3 headPosition = Camera.main.transform.position;
-        Vector3 gazeDirection = Camera.main.transform.forward;
+        Vector3 headPosition = transform.position;
+        Vector3 gazeDirection = transform.forward;
 
         RaycastHit hitInfo;
 
         if (Physics.Raycast(headPosition, gazeDirection, out hitInfo, maxDistance, Physics.DefaultRaycastLayers))
         {
-            // Raycast hit something
-            float distance = hitInfo.distance;
-            Debug.Log("Hit: " + hitInfo.collider.gameObject.name + ", Distance: " + distance);
+            // check if there is already a door, create a bigger colider on door
+            float angle = Vector3.Angle(hitInfo.normal, Vector3.up);
+            if (angle > 85 && angle < 95)
+            {
+
+                // Raycast hit something
+                float distance = hitInfo.distance;
+                Debug.Log("Hit: " + hitInfo.collider.gameObject.name + ", Distance: " + distance);
+
+                RaycastHit hitInfoFloor;
+                if(Physics.Raycast(hitInfo.point+hitInfo.normal*0.1f,Vector3.down, out hitInfoFloor))
+                {
+                Instantiate(prefab, hitInfoFloor.point, Quaternion.LookRotation(hitInfo.normal));
+                }
+            }
         }
         else
         {
