e54990a805d973c1c3c16dbe64aede91f19fd775
Charlotte-Pralong <chacha.pralong@gmail.com>
2023-04-26 13:24:55 +0200

    Add doors only every meter

--- unity/Assets/Samples/sasha-charlotte/RaycastFromHoloLens.cs
+++ unity/Assets/Samples/sasha-charlotte/RaycastFromHoloLens.cs
@@ -20,19 +20,37 @@ public class RaycastFromHoloLens : MonoBehaviour
 
         if (Physics.Raycast(headPosition, gazeDirection, out hitInfo, maxDistance, Physics.DefaultRaycastLayers))
         {
-            // check if there is already a door, create a bigger colider on door
+            // check if there is already a door, create a bigger collider on door
             float angle = Vector3.Angle(hitInfo.normal, Vector3.up);
             if (angle > 85 && angle < 95)
             {
-
                 // Raycast hit something
                 float distance = hitInfo.distance;
                 Debug.Log("Hit: " + hitInfo.collider.gameObject.name + ", Distance: " + distance);
 
                 RaycastHit hitInfoFloor;
-                if(Physics.Raycast(hitInfo.point+hitInfo.normal*0.1f,Vector3.down, out hitInfoFloor))
+                if (Physics.Raycast(hitInfo.point + hitInfo.normal * 0.1f, Vector3.down, out hitInfoFloor))
                 {
-                Instantiate(prefab, hitInfoFloor.point, Quaternion.LookRotation(hitInfo.normal));
+                    // Check if there is an existing prefab within 1 meter
+                    float checkDistance = 1.0f;
+                    Vector3 halfExtents = new Vector3(checkDistance, checkDistance, checkDistance);
+                    Collider[] nearbyColliders = Physics.OverlapBox(hitInfoFloor.point, halfExtents);
+
+                    bool existingPrefabFound = false;
+                    foreach (Collider collider in nearbyColliders)
+                    {
+                        if (collider.gameObject.name.StartsWith(prefab.name))
+                        {
+                            existingPrefabFound = true;
+                            break;
+                        }
+                    }
+
+                    // Instantiate the prefab only if no existing prefab is found within the specified distance
+                    if (!existingPrefabFound)
+                    {
+                        Instantiate(prefab, hitInfoFloor.point, Quaternion.LookRotation(hitInfo.normal));
+                    }
                 }
             }
         }
@@ -42,4 +60,5 @@ public class RaycastFromHoloLens : MonoBehaviour
             Debug.Log("No hit");
         }
     }
+
 }
